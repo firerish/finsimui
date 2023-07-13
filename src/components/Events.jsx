6cf8b4f45d2ee12ad5e06f850dd1745720b4b685
firerish <alexander.j.jordan@gmail.com>
2023-07-13 18:46:08 +0100

    Removed unused components, renamed LifeEvents.

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -7,7 +7,7 @@ import LifeEvent from "@/components/LifeEvent";
 export default function Events(props) {
     
     return (<><section className="p-4 border rounded">
-  <LifeEvent />
+  <Event />
 </section>
 </>);
 } 
